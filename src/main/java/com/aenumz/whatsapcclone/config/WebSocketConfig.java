2e40c0adefdbd57bf6255fbbc1c8bd348fbbeb0c
Adham Salem <adham.37.salem@gmail.com>
2025-12-28 21:33:33 +0300

    [Feature] Making WebSocket Service

---
+++ src/main/java/com/aenumz/whatsapcclone/config/WebSocketConfig.java
@@ -0,0 +1,81 @@
+package com.aenumz.whatsapcclone.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+
+import org.springframework.security.messaging.context.AuthenticationPrincipalArgumentResolver;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON;
+
+
+@Configuration
+@EnableWebSocket
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/user");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/users");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry
+                .addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:4200")
+                .withSockJS();
+    }
+
+    /**
+     * Add resolvers to support custom controller method argument types.
+     * <p>This does not override the built-in support for resolving handler
+     * method arguments. To customize the built-in support for argument
+     * resolution, configure {@code SimpAnnotationMethodMessageHandler} directly.
+     *
+     * @param argumentResolvers the resolvers to register (initially an empty list)
+     * @since 4.1.1
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new AuthenticationPrincipalArgumentResolver());
+    }
+
+    /**
+     * Configure the message converters to use when extracting the payload of
+     * messages in annotated methods and when sending messages (for example, through the
+     * "broker" SimpMessagingTemplate).
+     * <p>The provided list, initially empty, can be used to add message converters
+     * while the boolean return value is used to determine if default message should
+     * be added as well.
+     *
+     * @param messageConverters the converters to configure (initially an empty list)
+     * @return whether to also add default converter or not
+     */
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+        resolver.setDefaultMimeType(APPLICATION_JSON);
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        converter.setContentTypeResolver(resolver);
+        messageConverters.add(converter);
+        return false;
+    }
+
+
+}
