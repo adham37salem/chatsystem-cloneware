b8ec7d050afe02190af6f15e35aefc7ae9c5c39f
Adham Salem <adham.37.salem@gmail.com>
2026-01-02 02:15:05 +0300

    [Refactor] Add missing lastMessageTime for ChatMapper

--- src/main/java/com/aenumz/whatsapcclone/model/mapper/ChatMapper.java
+++ src/main/java/com/aenumz/whatsapcclone/model/mapper/ChatMapper.java
@@ -1,15 +1,18 @@
 package com.aenumz.whatsapcclone.model.mapper;
 
+import org.springframework.stereotype.Service;
+
 import com.aenumz.whatsapcclone.model.dto.chat.ChatResponse;
 import com.aenumz.whatsapcclone.model.entity.Chat;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ChatMapper {
     public ChatResponse toChatResponse(Chat chat, String senderId) {
-        return ChatResponse.builder()
+        return ChatResponse
+                .builder()
                 .id(chat.getId())
                 .name(chat.getChatName(senderId))
                 .unreadCount(chat.getUnreadMessageCount(senderId))
@@ -17,6 +20,7 @@ public class ChatMapper {
                 .isRecipientOnline(chat.getRecipient().isUserOnline())
                 .senderId(chat.getSender().getId())
                 .recipientId(chat.getRecipient().getId())
+                .lastMessageTime(chat.getLastMessageTime())
                 .build();
     }
 }
