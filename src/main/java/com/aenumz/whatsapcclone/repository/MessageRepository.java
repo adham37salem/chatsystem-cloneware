49be4ef79ea951fccd16ab70b6ba19f70c0edead
Adham Salem <adham.37.salem@gmail.com>
2026-01-07 00:05:23 +0300

    [Feature] Adding @Modifying Annotation for Set Messages Seen By Chat

--- src/main/java/com/aenumz/whatsapcclone/repository/MessageRepository.java
+++ src/main/java/com/aenumz/whatsapcclone/repository/MessageRepository.java
@@ -3,6 +3,7 @@ package com.aenumz.whatsapcclone.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -16,5 +17,6 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
     @Query(name = MessageConstant.FIND_MESSAGE_BY_CHAT_ID)
     List<Message> findMessagesByChatId(@Param("chatId") String chatId);
     @Query(name = MessageConstant.SET_MESSAGES_TO_SEEN_BY_CHAT)
+    @Modifying
     void setMessagesToSeenByChatId(@Param("chatId") String chatId, @Param("newState") MessageState state);
 }
